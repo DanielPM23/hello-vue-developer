91c904407170075ae5b6f5925931e7c2b242d17c
Daniel Palomino <danny345810@gmail.com>
2025-09-14 23:10:35 -0500

    feat(greetings): add developer count show component.

---
+++ src/greetings/presentation/components/developer-count-show.vue
@@ -0,0 +1,23 @@
+<script setup lang="js">
+import{toRefs} from 'vue';
+
+
+const props=defineProps({developerCount:{type:Number,required:true}});
+const {developerCount}=toRefs(props);
+</script>
+
+<template>
+<div>
+  <p>
+    developer registered: {{developerCount}}
+  </p>
+
+  <p>
+    <b>Note:</b> Unknown developers are not counted.
+  </p>
+</div>
+</template>
+
+<style scoped>
+
+</style>
